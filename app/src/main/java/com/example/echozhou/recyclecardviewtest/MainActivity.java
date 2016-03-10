c32958b9eff89b486c1d098df1aaf98c4da427f3
oo1993448102 <19931123xue@163.com>
2016-03-10 11:51:49 +0800

    recycleview

--- app/src/main/java/com/example/echozhou/recyclecardviewtest/MainActivity.java
+++ app/src/main/java/com/example/echozhou/recyclecardviewtest/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.echozhou.recyclecardviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecycleView();
+    }
 
+    private void initRecycleView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
     }
 }
---
+++ app/src/main/java/com/example/echozhou/recyclecardviewtest/MyAdapter.java
@@ -0,0 +1,7 @@
+package com.example.echozhou.recyclecardviewtest;
+
+/**
+ * Created by EchoZhou on 2016/3/10.
+ */
+public class MyAdapter {
+}
